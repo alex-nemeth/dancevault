7962dcc8934dbeb0a2f6ca020e5706d4c3acafdf
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-05-14 18:31:53 +0200

    tempotool init

---
+++ src/app/tempotool/tempotool.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tempotool',
+  templateUrl: './tempotool.component.html',
+  styleUrls: ['./tempotool.component.css'],
+})
+export class TempotoolComponent {
+  tempo: number = 0;
+}
