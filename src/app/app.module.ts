bfd77100c875e1467d2b284cc100245df44cef9e
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-05-14 18:32:00 +0200

    tempotool init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TempotoolComponent } from './tempotool/tempotool.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TempotoolComponent
   ],
   imports: [
     BrowserModule
